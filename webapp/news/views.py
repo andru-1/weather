401af2a5b858310c6f15a18404931560cac7163a
Andrey <andrukol@gmail.com>
2019-08-14 12:41:07 +0300

    Наследование шаблонов

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -9,4 +9,4 @@ def index():
     title = 'Прогноз погоды'
     weather = wether_by_city(current_app.config['WEATHER_DEFAULT_CITY'])
     news_list = News.query.order_by(News.published.desc()).all() # возврат  всех новостей из бд c сортировкой по дате
-    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+    return render_template('news/index.html', page_title=title, weather=weather, news_list=news_list)
